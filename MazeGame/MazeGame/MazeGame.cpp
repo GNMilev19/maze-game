01e243e8a2c3b45c2b0ccde3d6d61fad8d10f4ae
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2021-10-29 21:49:53 +0300

    Change VS toolset version

--- MazeGame/MazeGame/MazeGame.cpp
+++ MazeGame/MazeGame/MazeGame.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <conio.h>//We have this library,so that we can freely use the getch() function
-
 using namespace std;
+
 // #define is used to allow the programmer to give a name to a constant value before the program is compiled
 #define KEY_UP 72
 #define KEY_DOWN 80
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
-
-
 
 struct CELL {
     int playerY = 1;
@@ -17,7 +15,6 @@ struct CELL {
     bool isWall = false;
     bool isPlayer = false;
 };
-
 CELL playerCord;
 
 // this function generates the maze,by previously entered size
@@ -25,13 +22,13 @@ void printMaze(CELL** maze, int size, char free, char player) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             if (maze[i][j].isPlayer == true) {
-            cout << player << " ";
+                cout << player << " ";
             }
             else if (maze[i][j].isVisited == true) {
                 cout << free << " ";
             }
             else {
-                cout <<char(254) << " ";
+                cout << char(254) << " ";
             }
         }
         cout << endl;
@@ -100,46 +97,46 @@ void toVisited(CELL** maze, int* cordY, int* cordX, int dir, int* unvisitedCells
 // this function checks if the player is trying to move towards a wall
 void playerMovement(CELL** maze) {
     switch (_getch()) {
-        case KEY_UP: 
-            if (maze[playerCord.playerY - 1][playerCord.playerX].isWall == false) {
-                maze[playerCord.playerY][playerCord.playerX].isPlayer = false;
-                playerCord.playerY -= 1;
-                maze[playerCord.playerY][playerCord.playerX].isPlayer = true;
-            }
-            else {
-                cout << "Can't move there!";
-            }
-            break;
-        case KEY_DOWN: 
-            if (maze[playerCord.playerY + 1][playerCord.playerX].isWall == false) {
-                maze[playerCord.playerY][playerCord.playerX].isPlayer = false;
-                playerCord.playerY += 1;
-                maze[playerCord.playerY][playerCord.playerX].isPlayer = true;
-            }
-            else {
-                cout << "Can't move there!";
-            }
-            break;
-        case KEY_LEFT: 
-            if (maze[playerCord.playerY][playerCord.playerX - 1].isWall == false) {
-                maze[playerCord.playerY][playerCord.playerX].isPlayer = false;
-                playerCord.playerX -= 1;
-                maze[playerCord.playerY][playerCord.playerX].isPlayer = true;
-            }
-            else {
-                cout << "Can't move there!";
-            }
-            break;
-        case KEY_RIGHT: 
-            if (maze[playerCord.playerY][playerCord.playerX + 1].isWall == false) {
-                maze[playerCord.playerY][playerCord.playerX].isPlayer = false;
-                playerCord.playerX += 1;
-                maze[playerCord.playerY][playerCord.playerX].isPlayer = true;
-            }
-            else {
-                cout << "Can't move there!";
-            }
-            break;
+    case KEY_UP:
+        if (maze[playerCord.playerY - 1][playerCord.playerX].isWall == false) {
+            maze[playerCord.playerY][playerCord.playerX].isPlayer = false;
+            playerCord.playerY -= 1;
+            maze[playerCord.playerY][playerCord.playerX].isPlayer = true;
+        }
+        else {
+            cout << "Can't move there!";
+        }
+        break;
+    case KEY_DOWN:
+        if (maze[playerCord.playerY + 1][playerCord.playerX].isWall == false) {
+            maze[playerCord.playerY][playerCord.playerX].isPlayer = false;
+            playerCord.playerY += 1;
+            maze[playerCord.playerY][playerCord.playerX].isPlayer = true;
+        }
+        else {
+            cout << "Can't move there!";
+        }
+        break;
+    case KEY_LEFT:
+        if (maze[playerCord.playerY][playerCord.playerX - 1].isWall == false) {
+            maze[playerCord.playerY][playerCord.playerX].isPlayer = false;
+            playerCord.playerX -= 1;
+            maze[playerCord.playerY][playerCord.playerX].isPlayer = true;
+        }
+        else {
+            cout << "Can't move there!";
+        }
+        break;
+    case KEY_RIGHT:
+        if (maze[playerCord.playerY][playerCord.playerX + 1].isWall == false) {
+            maze[playerCord.playerY][playerCord.playerX].isPlayer = false;
+            playerCord.playerX += 1;
+            maze[playerCord.playerY][playerCord.playerX].isPlayer = true;
+        }
+        else {
+            cout << "Can't move there!";
+        }
+        break;
     }
 }
 
@@ -153,11 +150,11 @@ bool freeCheck(int dir, int size, int cordY, int cordX) {
 //This function is used to display a text,when/if you win the game.
 void winningText() {
     cout << "   __     __                    _       _ \n"
-            "   \\ \\   / /                   (_)     | | \n"
-            "    \\ \\_/ /__  _   _  __      ___ _ __ | | \n"
-            "     \\   / _ \\| | | | \\ \\ /\\ / / | '_ \\| | \n"
-            "      | | (_) | |_| |  \\ V  V /| | | | |_| \n"
-            "      |_|\\___/ \\__,_|   \\_/ \\_/|_|_| |_(_) \n";
+        "   \\ \\   / /                   (_)     | | \n"
+        "    \\ \\_/ /__  _   _  __      ___ _ __ | | \n"
+        "     \\   / _ \\| | | | \\ \\ /\\ / / | '_ \\| | \n"
+        "      | | (_) | |_| |  \\ V  V /| | | | |_| \n"
+        "      |_|\\___/ \\__,_|   \\_/ \\_/|_|_| |_(_) \n";
 }
 
 // this is the main function
@@ -183,7 +180,7 @@ int main() {
         if (size <= 0) {
             cout << "You should enter positive number!" << endl;
         }
-     } while (size <= 0);
+    } while (size <= 0);
     system("cls");
 
     cordY = 1;
