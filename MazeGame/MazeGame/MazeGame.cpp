5ddea7f19be2c87870a73cebcb42aebc91a6347b
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2021-11-06 18:10:19 +0200

    Clear code

--- MazeGame/MazeGame/MazeGame.cpp
+++ MazeGame/MazeGame/MazeGame.cpp
@@ -19,17 +19,6 @@ struct CELL {
 };
 CELL playerCord;
 
-void color(int color) {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
-
-void gotoxy(int x, int y) {
-	COORD c;
-	c.X = x;
-	c.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-
 void menu(string arrow, int arrowPos) {
 
 	cout << "*--------------------------------------*" << endl;
